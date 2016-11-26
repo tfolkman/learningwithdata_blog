a21cd4ceecae06a2177e0052f965bc45d7555afa
Tyler Folkman <tylerfolkman@gmail.com>
2016-11-25 20:43:02 -0700

    added posts

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Tyler Folkman'
 SITENAME = u'Learning With Data'
-SITEURL = ''
+SITEURL = 'http://learningwithdata.com'
 SITETITLE = AUTHOR
 SITESUBTITLE = 'Data Scientist @Ancestry'
 SITELOGO = '/images/profile.jpg'
@@ -14,6 +14,8 @@ MARKUP = ('md', 'ipynb')
 PLUGIN_PATHS = ['./plugins']
 PLUGINS = ['ipynb.markup']
 
+ROBOTS = 'index, follow'
+
 PATH = 'content'
 
 TIMEZONE = 'MST'
@@ -21,11 +23,17 @@ TIMEZONE = 'MST'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+
+MENUITEMS =  (('Categories', '/categories.html'),
+	     ('Tags', '/tags.html'),)
 
 # Blogroll
 #LINKS = (('Pelican', 'http://getpelican.com/')
@@ -34,7 +42,8 @@ AUTHOR_FEED_RSS = None
 SOCIAL = (('linkedin', 'https://www.linkedin.com/in/tylerfolkman'),
           ('github', 'https://github.com/tfolkman'),
           ('envelope-o', 'mailto:learningwithdata@gmail.com'),
-          ('twitter', 'https://twitter.com/tyler_folkman'))
+          ('twitter', 'https://twitter.com/tyler_folkman'),
+          ('rss', 'feeds/all.atom.xml'))
 
 DEFAULT_PAGINATION = 10
 
